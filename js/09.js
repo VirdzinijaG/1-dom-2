8863bed3182dd9b287a7faebc9870d66373e4704
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-07-13 21:16:55 +0300

    09 uzduotis

--- js/09.js
+++ js/09.js
@@ -6,22 +6,22 @@ const numberDOM = document.querySelector(".number");
 const resetDOM = document.querySelector(".reset");
 const ulDOM = document.querySelector('ul');
 
-ulDOM.innerHTML = "Tu pasileidai zaidima - sekmes!";
+ulDOM.innerText = "Tu pasileidai zaidima - sekmes!";
 
 function minusClick() {
     numberDOM.innerText = --rezultatas;
-    ulDOM.innerHTML = "Paspaudei minusa ir dabartinis rezultatas yra " + rezultatas;
+    ulDOM.innerText = "Paspaudei minusa ir dabartinis rezultatas yra " + rezultatas;
 }
 
 function plusCick() {
     numberDOM.innerText = ++rezultatas;
-    ulDOM.innerHTML = "Paspaudei pliusa ir dabartinis rezultatas yra " + rezultatas;
+    ulDOM.innerText = "Paspaudei pliusa ir dabartinis rezultatas yra " + rezultatas;
 }
 
 function resetClick() {
     rezultatas = 0;
     numberDOM.innerText = rezultatas;
-    ulDOM.innerHTML = "Paspaudei reset ir dabartinis rezultatas yra " + rezultatas;
+    ulDOM.innerText = "Paspaudei reset ir dabartinis rezultatas yra " + rezultatas;
 }
 
 minusDOM.addEventListener('click', minusClick);
