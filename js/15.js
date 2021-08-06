6d9e85b934ff53db8a0551926f9f9a48ba29d96a
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-06 13:18:17 +0300

    font_awesome, 15 uzduotis

---
+++ js/15.js
@@ -0,0 +1,61 @@
+function generuotiSocials(selector, iconsList) {
+    // validation
+    if (typeof selector !== 'string' || selector === '') {
+      console.error(
+        'ERROR: selector turi buti ne tuscias stringas',
+        selector,
+        iconsList
+      );
+      return false;
+    }
+  
+    const DOM = document.querySelector(selector);
+    if (!DOM) {
+      console.error(
+        'ERROR: pagal pateikta selector nepavyko rasti jokio DOM elemento'
+      );
+      return false;
+    }
+  
+    if (!Array.isArray(iconsList) || iconsList.length === 0) {
+      console.error('ERROR: iconsList turi buti ne tuscias array');
+      return false;
+    }
+  
+    // logic
+    let HTML = '';
+    const availableIcons = [
+      'facebook',
+      'twitter',
+      'linkedin',
+      'instagram',
+      'pinterest',
+      'amazon'
+    ];
+  
+    for (const icon of iconsList) {
+      if (
+        typeof icon === 'string' &&
+        icon !== '' &&
+        availableIcons.includes(icon)
+      ) {
+        HTML += `<i class="fa fa-${icon}"></i>`;
+      }
+    }
+  
+    // post-logic validation
+    if (HTML === '') {
+      console.error(
+        'ERROR: iconsList turi tureti bent viena elementa kuris buti ne tuscias tekstas'
+      );
+      return false;
+    }
+  
+    // result return
+    DOM.innerHTML = HTML;
+  }
+  
+  const icons = ['facebook', 'instagram', 'twitter', 'linkedin', 'pinterest', 'amazon'];
+  
+  generuotiSocials('.socials', icons);
+  
